5ad0948a0307fd43e0dce732a09dd909f7de5c8a
Effectivement <richel265@tuta.io>
2026-04-08 21:36:18 -0400

    Add basic template, and copy over code from Hello example

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use rocket_dyn_templates::{Template, context};
+
+#[macro_use]
+extern crate rocket;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(FromFormField)]
+enum Lang {
+    #[field(value = "en")]
+    English,
+    #[field(value = "ru")]
+    #[field(value = "ру")]
+    Russian,
+}
+
+#[derive(FromForm)]
+struct Options<'r> {
+    emoji: bool,
+    name: Option<&'r str>,
+}
+
+#[derive(FromForm)]
+struct HabitOptions<'r> {
+    description: Option<&'r str>,
+    tag: Option<&'r str>,
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/hello/world
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/hello/мир
+#[get("/мир")]
+fn mir() -> &'static str {
+    "Привет, мир!"
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/wave/Rocketeer/100
+#[get("/<name>/<age>", rank = 2)]
+fn wave(name: &str, age: u8) -> String {
+    format!("👋 Hello, {} year old named {}!", age, name)
+}
+
+// Note: without the `..` in `opt..`, we'd need to pass `opt.emoji`, `opt.name`.
+//
+// Try visiting:
+//   http://127.0.0.1:8000/?emoji
+//   http://127.0.0.1:8000/?name=Rocketeer
+//   http://127.0.0.1:8000/?lang=ру
+//   http://127.0.0.1:8000/?lang=ру&emoji
+//   http://127.0.0.1:8000/?emoji&lang=en
+//   http://127.0.0.1:8000/?name=Rocketeer&lang=en
+//   http://127.0.0.1:8000/?emoji&name=Rocketeer
+//   http://127.0.0.1:8000/?name=Rocketeer&lang=en&emoji
+//   http://127.0.0.1:8000/?lang=ru&emoji&name=Rocketeer
+#[get("/?<lang>&<opt..>")]
+fn hello(lang: Option<Lang>, opt: Options<'_>) -> String {
+    let mut greeting = String::new();
+    if opt.emoji {
+        greeting.push_str("👋 ");
+    }
+
+    match lang {
+        Some(Lang::Russian) => greeting.push_str("Привет"),
+        Some(Lang::English) => greeting.push_str("Hello"),
+        None => greeting.push_str("Hi"),
+    }
+
+    if let Some(name) = opt.name {
+        greeting.push_str(", ");
+        greeting.push_str(name);
+    }
+
+    greeting.push('!');
+    greeting
+}
+
+#[get("/?<name>&<opt..>")]
+fn add_habit(name: String, opt: HabitOptions<'_>) {}
+// render main tracker
+#[get("/")]
+fn main_page() -> Template {
+    Template::render("index", context! {})
+}
+
+// fn new_habit(habit: Habit<'_>) ->
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![main_page])
+        .mount("/add", routes![add_habit])
+        .mount("/hello", routes![world, mir])
+        .mount("/wave", routes![wave])
+        .attach(Template::fairing())
+}
